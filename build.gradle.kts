426c250534c139c62ba3f1f0bc0fb2d21bd92a61
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-09-05 16:14:43 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 
 android {
     namespace = "com.example.memorycue"
